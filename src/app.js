981b01c0134174abab35e0c4a93b473ee8c36050
Stiv <pabloml@kzsoftworks.com>
2018-10-15 23:15:01 -0300

    Refactor pet component and create api call

--- src/app.js
+++ src/app.js
@@ -1,28 +1,57 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Song from './shoe';
-import SpotifyWebApi from 'spotify-web-api-node';
+import pf from 'petfinder-client';
+import Pet from './pet';
 
-const spotifyWebApi = new SpotifyWebApi({
-  clientId: '83885fa4c9384bebbe01f891ed4204c4',
-  clientSecret: '7680b9c440fd4164abd785a42833c870'
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
 });
-spotifyWebApi.setAccessToken(
-  'BQAzmVLe96umDNHH58YpCn7UTjTiq-gWyRfZap6kxjQCbC_T3viUCc7YABf3v5NxqzRFn9v6KhOca7LCJb8'
-);
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
   componentDidMount() {
-    const tracksResponse = spotifyWebApi.searchTracks('Bad Bunny');
-    tracksResponse.then(data => console.log(data));
+    petfinder.pet
+      .find({ output: 'full', location: 'New York, NY' })
+      .then(data => {
+        let pets = [];
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        }
+        this.setState({
+          pets
+        });
+      });
   }
   render() {
     return (
       <div>
-        <h1> Shoe Store </h1>
-        <Song name="Yeezy Boost 350" shape="Sneaker" color="white" />
-        <Song name="Jordan 1" shape="Sneaker" color="red" />
-        <Song name="Gucci Canvas" shape="simple sole" color="green" />
+        {this.state.pets.map(pet => {
+          let breed;
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(', ');
+          } else {
+            breed = pet.breeds.breed;
+          }
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city}, ${pet.contact.state} `}
+            />
+          );
+        })}
       </div>
     );
   }
